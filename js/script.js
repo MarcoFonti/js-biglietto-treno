baef14dd4aed37c2e01fab96abc59e6730aa1b70
MarcoFonti <theemporiumg@gmail.com>
2023-12-18 18:08:16 +0100

    add changes to if and else

--- js/script.js
+++ js/script.js
@@ -1,32 +1,37 @@
 //Check
 console.log('JS OK');
 
+
 //Chiedere all'utente il numero di chilometri che vuole percorre
 const numberKilo = parseInt(prompt('Quanti chilometri vuoi percorre?', '20'));
 //Chiedere all'utente età
-const userAge = parseInt(prompt('Quanti anni hai?', '24'));
+const userAge = parseInt(prompt('Quanti anni hai?', '17'));
 //Risposta in console
 console.log(numberKilo, userAge);
 
+
 //Calcolo km utente
-const calculationKilo = numberKilo * 0.21;
+let calculationKilo = numberKilo * 0.21;
 console.log ('Calcolo: ' + calculationKilo);
 
 
 //Apllico sconto solo se:
+let result;
 if (userAge < 18) {
-    console.log ('Avrà uno sconto del 20%');
+    result = 'e avrai uno sconto del 20%';
 } else if (userAge > 65) {
-    console.log ('Avrà uno sconto del 40%');
+    result = 'e avrai uno sconto del 40%';
 } else {
-    console.log('Non avrà sconto');
+    result = 'ma non avrai lo sconto';
 }
+
 
 //Massimo 2 decimali
 const formattedCalc = calculationKilo.toFixed(2);
+
 
 //Creo messaggio visivo
 const element = document.getElementById('price');
 const messPrice = ' Il prezzo del biglietto è ';
 
-price.innerText = messPrice + formattedCalc;
+price.innerText = messPrice + ' ' + formattedCalc + '€' +  ' ' + result;
