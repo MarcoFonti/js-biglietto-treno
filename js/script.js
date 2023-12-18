6c6ccbe778172718e32aa2381a4a5c8568d4feae
MarcoFonti <theemporiumg@gmail.com>
2023-12-18 17:08:42 +0100

    add exercise

--- js/script.js
+++ js/script.js
@@ -1,2 +1,32 @@
 //Check
 console.log('JS OK');
+
+//Chiedere all'utente il numero di chilometri che vuole percorre
+const numberKilo = parseInt(prompt('Quanti chilometri vuoi percorre?', '20'));
+//Chiedere all'utente età
+const userAge = parseInt(prompt('Quanti anni hai?', '24'));
+//Risposta in console
+console.log(numberKilo, userAge);
+
+//Calcolo km utente
+const calculationKilo = numberKilo * 0.21;
+console.log ('Calcolo: ' + calculationKilo);
+
+
+//Apllico sconto solo se:
+if (userAge < 18) {
+    console.log ('Avrà uno sconto del 20%');
+} else if (userAge > 65) {
+    console.log ('Avrà uno sconto del 40%');
+} else {
+    console.log('Non avrà sconto');
+}
+
+//Massimo 2 decimali
+const formattedCalc = calculationKilo.toFixed(2);
+
+//Creo messaggio visivo
+const element = document.getElementById('price');
+const messPrice = ' Il prezzo del biglietto è ';
+
+price.innerText = messPrice + formattedCalc;
---
+++ textPassages.md
@@ -0,0 +1,13 @@
+# Testo Passaggi
+
+1 Chiedere all'utente il numero di chilometri che vuole percorre e l'età
+
+2 Calcolare i km dell'utente per 0.21€ km 
+
+3 Applicare sconto del 20% se minorenne 
+
+4 Applicare sconto del 40% se over 65
+
+5 Mettere prezzo in forma umana
+
+6 Creo messaggio visivo
