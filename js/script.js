b5533d7c2712b779e3b8455d951978aa1e3b47fe
MarcoFonti <theemporiumg@gmail.com>
2024-04-06 15:23:42 +0200

    add commit

--- js/script.js
+++ js/script.js
@@ -2,39 +2,59 @@
 console.log('JS OK');
 
 
-//Chiedere all'utente il numero di chilometri che vuole percorre
+/* CREO UNA VARIBILE E GLI ASSEGNO LA RISPOSTA DELL'UTENTE */
 const numberKilo = parseInt(prompt('Quanti chilometri vuoi percorre?', 20));
-//Chiedere all'utente età
+
+
+/* CREO UNA VARIBILE E GLI ASSEGNO IL VALORE DELL'UTENTE */
 const userAge = parseInt(prompt('Quanti anni hai?', 17));
-//Risposta in console
+
+
+/* VERIFICO IL SALVATAGGIO DELLE RISPOSTE */
 console.log(numberKilo, userAge);
 
-//Recupore l'elemento
+
+/* RECUPERO ID HTML */
 const priceElement = document.getElementById('price');
 
-// Preparo il messaggio
+
+/* CREO UNA VARIBILE CON IL MESSAGGIO */
 let message = 'Il prezzo del biglietto è ';
 
-//Calcolo km utente
+
+/* CREO UNA VARIBILE IN BASE AI KM (0.21€ A KM) */
 const priceKm= 0.21;
 
-//Prezzo senza sconto
+
+/* CREO UNA VARIABILE E GLI ASSEGNO IL NUMERO DI KM CHE MI HA DATO UTENTE * 0.21 */
 const priceWithoutDisc = numberKilo * priceKm;
+/* CONTROLLO LA RISPOSTA */
 console.log(priceWithoutDisc);
 
-//Sconti
+
+/* CREO DUE VARIBILE CON I VARI SCONTI */
 let scount20 = 0.20;
 let scount40= 0.40;
 
+
+/* CREO UNA VARIBILE ESTERNA PER MANIPOLARLA DENTRO LA CONDIZIONE E GLI ASSEGNO IL PREZZO SENZA SCONTO */
 let finalPrice = priceWithoutDisc;
 
+
+/* SE ETA' DELL'UTENTE E' MINORE DI 18  */
 if (userAge < 18) {
+    /* PREZZO E SCONTO */
     finalPrice = priceWithoutDisc * (1 - scount20);
+
+    /* ALTRIMENTI SE ETA' E' MAGGIORE DI 65 */
 } else if (userAge > 65) {
+
+    /* PREZZO SCONTO */
     finalPrice = priceWithoutDisc * (1 - scount40);
 }
 
-// Messaggio del prezzo
+
+/* STAMPO IN PAGINA IL MEAGGIO + IL PREZZO IN BASE ALL'ETA' */
 priceElement.innerText = message + finalPrice.toFixed(2) + '€';
 
 
